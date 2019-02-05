33cf9957916a6fe0ed1cdf4bdf1b507b5c1fa6ea
Thomas Rasnom <tomx.live@hotmail.co.uk>
2019-02-05 17:33:45 +0000

    Update grinch.sh
    changed comments

--- grinch.sh
+++ grinch.sh
@@ -1,5 +1,5 @@
-#/Library/Caches/com.apple.desktop.admin.png
+#checks if the christmas background exists and if so removes it.
 [ -e /Library/Caches/com.apple.desktop.admin.png ] && rm /Library/Caches/com.apple.desktop.admin.png
-echo "it worked"
+#checks if the mojave dynamic wallpaper exists.
 [ -e '/Library/Desktop Pictures/Mojave.heic' ] && rm '/Library/Desktop Pictures/Mojave.heic'
-#/Library/Desktop Pictures/Mojave.heic
+
