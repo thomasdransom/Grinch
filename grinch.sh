1dce621243484cd52e824749a204222fdee41646
Thomas Rasnom <tomx.live@hotmail.co.uk>
2019-02-05 17:31:35 +0000

    Add files via upload

---
+++ grinch.sh
@@ -0,0 +1,5 @@
+#/Library/Caches/com.apple.desktop.admin.png
+[ -e /Library/Caches/com.apple.desktop.admin.png ] && rm /Library/Caches/com.apple.desktop.admin.png
+echo "it worked"
+[ -e '/Library/Desktop Pictures/Mojave.heic' ] && rm '/Library/Desktop Pictures/Mojave.heic'
+#/Library/Desktop Pictures/Mojave.heic
