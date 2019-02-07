1d2b79e100d8be26a1c672ad8e093290d2f156e1
Thomas Rasnom <tomx.live@hotmail.co.uk>
2019-02-07 16:12:05 +0000

    Update grinch.sh
    Surrounded the important parts of the code in a if else, to stop potential errors when the background isnt installed.

--- grinch.sh
+++ grinch.sh
@@ -1,5 +1,14 @@
-#checks if the christmas background exists and if so removes it.
-[ -e /Library/Caches/com.apple.desktop.admin.png ] && rm /Library/Caches/com.apple.desktop.admin.png
-#checks if the mojave dynamic wallpaper exists.
-[ -e '/Library/Desktop Pictures/Mojave.heic' ] && rm '/Library/Desktop Pictures/Mojave.heic'
-
+#checks if the picture still exists
+if [[ -e /Library/Caches/com.apple.desktop.admin.png ]]
+#removes the background
+then rm /Library/Caches/com.apple.desktop.admin.png
+echo "it worked"
+else echo "No background to delete"
+fi
+#checks if the mojave background exists
+if [[ -e '/Library/Desktop Pictures/Mojave.heic' ]]
+#removes the mojave background
+then rm '/Library/Desktop Pictures/Mojave.heic'
+else echo "No mojave background installed"
+fi
+#/Library/Desktop Pictures/Mojave.heic
