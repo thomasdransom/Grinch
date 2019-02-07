ff7fadb544272f60d9703fc334affc21f285287d
Thomas Rasnom <tomx.live@hotmail.co.uk>
2019-02-07 16:13:36 +0000

    Update grinch.sh
    Added my name at the top

--- grinch.sh
+++ grinch.sh
@@ -1,8 +1,8 @@
+#Thomas Ransom Feb 2019
 #checks if the picture still exists
 if [[ -e /Library/Caches/com.apple.desktop.admin.png ]]
 #removes the background
 then rm /Library/Caches/com.apple.desktop.admin.png
-echo "it worked"
 else echo "No background to delete"
 fi
 #checks if the mojave background exists
